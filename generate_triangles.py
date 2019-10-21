216775e700193e5e820a59ca4b9ca6208efcb318
Clara Kang <heng.kang@mail.mcgill.ca>
2019-10-21 01:36:19 -0400

    delaunay triangulation

--- generate_triangles.py
+++ generate_triangles.py
@@ -74,6 +74,8 @@ def getSignedAngle(vec1, vec2):
     # inkex.debug("vec1: " + str(vec1))
     # inkex.debug("vec2: " + str(vec2))
     a = np.dot(vec1, vec2) / ( np.linalg.norm(vec1)  * np.linalg.norm(vec2))
+    if a >= 1: # vector parallel, angle is 0
+        return 0
     angle = math.acos(np.dot(vec1, vec2) / ( np.linalg.norm(vec1)  * np.linalg.norm(vec2)))
     # inkex.debug("angle: " + str(angle * 180 / 3.14159))
     vec1_3d = np.append(vec1, 0)
@@ -186,7 +188,7 @@ def generatePoints(svg_path, spacing):
 
         path_len = getPathLen(path)
         segs_num = math.floor(path_len/spacing)
-        
+
         if segs_num == 0:
             continue
 
@@ -311,17 +313,8 @@ class Pattern(inkex.Effect):
             p = Popen('inkscape --query-%s --query-id=%s "%s"' % (query, self.options.ids[0], self.args[-1]), shell=True, stdout=PIPE, stderr=PIPE)
             rc = p.wait()
             q[query] = scale*float(p.stdout.read())
-        # mat = simpletransform.composeParents(self.selected[self.options.ids[0]], [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
 
-        # defs = self.xpathSingle('/svg:svg//svg:defs')
-        # pattern = inkex.etree.SubElement(defs ,inkex.addNS('pattern','svg'))
-        # pattern.set('id', 'points' + str(random.randint(1, 9999)))
-        # pattern.set('width', str(q['width']))
-        # pattern.set('height', str(q['height']))
-        # pattern.set('patternTransform', 'translate(%s,%s)' % (q['x'] - mat[0][2], q['y'] - mat[1][2]))
-        # pattern.set('patternUnits', 'userSpaceOnUse')
-
-        # # generate random pattern of points
+        # generate random pattern of points
         node = self.selected.values()[0]
         path_string = node.attrib[u'd']
         svg_path = simplepath.parsePath(path_string)
@@ -351,56 +344,29 @@ class Pattern(inkex.Effect):
             attribs = {'cx': str(point.loc[0]), 'cy': str(point.loc[1]), 'r':str(1.0), 'style': style}
             inkex.etree.SubElement(points_group, inkex.addNS('circle', 'svg'), attribs)
 
-        # c = voronoi.Context()
-        # pts = []
-        # b = float(self.options.border)          # width of border
-        # for i in range(-1, int(q['width']/self.options.size + 2)):
-        #     for j in range(-1, int(q['height']/self.options.size + 2)):
-        #         offset_x = random.random() * self.options.size * 0.25
-        #         offset_y = random.random() * self.options.size * 0.25
-        #         pts.append(voronoi.Site(self.options.size * i + offset_x, self.options.size* j + offset_y))
-        # if len(pts) < 3:
-        #     inkex.errormsg("Please choose a larger object, or smaller cell size")
-        #     exit()
-        #
-        # # plot Voronoi diagram
-        # sl = voronoi.SiteList(pts)
-        # c.triangulate = True
-        # voronoi.voronoi(sl, c)
-        # path = ""
-        # for triangle in c.triangles:
-        #     inkex.debug("triangle: " + str(triangle[0]) + ", " + str(triangle[1]) + ", " + str(triangle[2]))
-        #     for i in range(0,3):
-        #         pt1 = triangle[i]
-        #         pt2 = triangle[(i+1)%3]    # two vertices
-        #         [x1, y1, x2, y2] = clip_line(pts[pt1].x, pts[pt1].y, pts[pt2].x, pts[pt2].y, q['width'], q['height'])
-        #         # [x1, y1, x2, y2] = clip_line(c.vertices[pt1][0], c.vertices[pt1][1], c.vertices[pt2][0], c.vertices[pt2][1], q['width'], q['height'])
-        #         path += 'M %.3f,%.3f %.3f,%.3f' % (x1, y1, x2, y2)
-        #
-        # patternstyle = {'stroke': '#000000', 'stroke-width': str(scale)}
-        # attribs = {'d': path, 'style': simplestyle.formatStyle(patternstyle)}
-        # inkex.etree.SubElement(pattern, inkex.addNS('path', 'svg'), attribs)
-        #
-        # link selected object to pattern
-        # obj_styles = simplestyle.parseStyle(node.attrib[u'style'])
-        # inkex.debug("obj_styles: " + str(obj_styles))
-        # obj_styles[u'fill'] = u'url(#' + str(pattern.get('id')) + ')'
-        # node.attrib[u'style'] = simplestyle.formatStyle(obj_styles)
+        c = voronoi.Context()
+        pts_to_trig = []
+        for point in pts:
+            pts_to_trig.append(voronoi.Site(point.loc[0], point.loc[1]))
 
-        # link selected object to pattern
-        # obj = self.selected[self.options.ids[0]]
-        # style = {}
-        # if obj.attrib.has_key('style'):
-        #     style = simplestyle.parseStyle(obj.attrib['style'])
-        # style['fill'] = 'url(#%s)' % pattern.get('id')
-        # obj.attrib['style'] = simplestyle.formatStyle(style)
-        # if obj.tag == inkex.addNS('g', 'svg'):
-        #     for node in obj:
-        #         style = {}
-        #         if node.attrib.has_key('style'):
-        #             style = simplestyle.parseStyle(node.attrib['style'])
-        #         style['fill'] = 'url(#%s)' % pattern.get('id')
-        #         node.attrib['style'] = simplestyle.formatStyle(style)
+        # triangulate with library
+        sl = voronoi.SiteList(pts_to_trig)
+        c.triangulate = True
+        voronoi.voronoi(sl, c)
+
+        # group for triangles
+        triangles_group = inkex.etree.SubElement(points_layer, inkex.addNS('g', 'svg'))
+
+        style = "fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+        for triangle in c.triangles:
+            inkex.debug("triangle: " + str(triangle[0]) + ", " + str(triangle[1]) + ", " + str(triangle[2]))
+            verts = [pts_to_trig[triangle[i]] for i in range(3)]
+            path = 'M %.3f,%.3f %.3f,%.3f %.3f,%.3f z' % (verts[0].x, verts[0].y, verts[1].x, verts[1].y, verts[2].x, verts[2].y)
+
+            attribs = {'d': path, 'style': style}
+            triangle_path = inkex.etree.SubElement(triangles_group, inkex.addNS('path', 'svg'), attribs)
+            triangle_path.set("{%s}connector-curvature" % inkex.NSS[u'inkscape'], "0")
+
 if __name__ == '__main__':
     e = Pattern()
     e.affect()
